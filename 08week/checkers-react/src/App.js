112c692f3a58bea1476dbaf3072f6f9d02f4ca40
Collin Betori <cbetori@gmail.com>
2019-03-01 17:17:58 -0600

    react Checkers

---
+++ 08week/checkers-react/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Container } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+      <Container className="App">
+
+      </Container>
+    );
+  }
+}
+
+export default App;
